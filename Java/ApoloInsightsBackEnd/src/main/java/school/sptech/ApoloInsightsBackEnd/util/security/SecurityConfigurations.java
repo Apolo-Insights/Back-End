47eb40fefdd3631db4d80f11f934ccc8f6735b42
JuanMeloOli <Juan.Oliveira@sptech.school>
2025-04-27 13:11:13 -0300

    usuario -> usuarios

--- Java/ApoloInsightsBackEnd/src/main/java/school/sptech/ApoloInsightsBackEnd/util/security/SecurityConfigurations.java
+++ Java/ApoloInsightsBackEnd/src/main/java/school/sptech/ApoloInsightsBackEnd/util/security/SecurityConfigurations.java
@@ -30,7 +30,7 @@ public class SecurityConfigurations {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.POST, "/login", "/usuario").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/login", "/usuarios").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
