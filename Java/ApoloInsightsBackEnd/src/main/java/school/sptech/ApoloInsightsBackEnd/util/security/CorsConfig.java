cda5905d8c33499b66f81ddc889a91558e533b75
JuanMeloOli <Juan.Oliveira@sptech.school>
2025-04-27 14:32:38 -0300

    teste

--- Java/ApoloInsightsBackEnd/src/main/java/school/sptech/ApoloInsightsBackEnd/util/security/CorsConfig.java
+++ Java/ApoloInsightsBackEnd/src/main/java/school/sptech/ApoloInsightsBackEnd/util/security/CorsConfig.java
@@ -3,11 +3,9 @@ package school.sptech.ApoloInsightsBackEnd.util.security;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
 
     @Value("${cors.allowed.origin}")
