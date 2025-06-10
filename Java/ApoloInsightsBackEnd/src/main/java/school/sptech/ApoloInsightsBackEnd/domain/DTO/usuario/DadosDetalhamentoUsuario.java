55680f5f948b38e570e929d648161fcf5ccdc304
JoaoKeviin <joao.matos@sptech.school>
2025-06-10 20:24:14 -0300

    fix: incluindo dados

--- Java/ApoloInsightsBackEnd/src/main/java/school/sptech/ApoloInsightsBackEnd/domain/DTO/usuario/DadosDetalhamentoUsuario.java
+++ Java/ApoloInsightsBackEnd/src/main/java/school/sptech/ApoloInsightsBackEnd/domain/DTO/usuario/DadosDetalhamentoUsuario.java
@@ -4,6 +4,7 @@ import school.sptech.ApoloInsightsBackEnd.domain.Role;
 import school.sptech.ApoloInsightsBackEnd.domain.Usuario;
 
 public record DadosDetalhamentoUsuario(
+        Long id,
         String nome,
         String cpf,
         String email,
@@ -12,6 +13,7 @@ public record DadosDetalhamentoUsuario(
 ) {
     public DadosDetalhamentoUsuario(Usuario dados) {
         this(
+                dados.getId(),
                 dados.getNome(),
                 dados.getCpf(),
                 dados.getEmail(),
--- Java/ApoloInsightsBackEnd/src/main/java/school/sptech/ApoloInsightsBackEnd/security/TokenService.java
+++ Java/ApoloInsightsBackEnd/src/main/java/school/sptech/ApoloInsightsBackEnd/security/TokenService.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import school.sptech.ApoloInsightsBackEnd.domain.Usuario;
+
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
@@ -19,7 +20,7 @@ public class TokenService {
     private static final Logger logger = LoggerFactory.getLogger(TokenService.class);
     private static final String ISSUER = "API Apolo Insights";
     private static final String TOKEN_ERROR = "Token JWT inválido ou expirado!";
-    private static final String GENERATION_ERROR = "ERRO AO GERAR O TOKEN JWT";
+    private static final String GENERATION_ERROR = "Erro ao gerar o token JWT";
 
     @Value("${api.security.token.secret}")
     private String secret;
@@ -27,49 +28,48 @@ public class TokenService {
     @Value("${api.security.token.expiration-time}")
     private Integer expirationTime;
 
-    public String gerarToken(Usuario usuario){
+    public String gerarToken(Usuario usuario) {
         try {
-            var algoritmo = Algorithm.HMAC256(secret);
-            var jwtBuilder = JWT.create()
+            // Algoritmo para codificar o JWT
+            Algorithm algoritmo = Algorithm.HMAC256(secret);
+
+            // Construção do JWT com claims relevantes
+            return JWT.create()
                     .withIssuer(ISSUER)
                     .withSubject(usuario.getEmail())
                     .withClaim("nome", usuario.getNome())
                     .withClaim("id", usuario.getId().toString())
-                    .withClaim("email", usuario.getEmail());
-
-            if (usuario.getTelefone() != null) {
-                jwtBuilder.withClaim("telefone", usuario.getTelefone());
-            }
-
-            if (usuario.getGenero() != null) {
-                jwtBuilder.withClaim("genero", usuario.getGenero().toString());
-            }
-
-            return jwtBuilder
+                    .withClaim("email", usuario.getEmail())
+                    .withClaim("telefone", usuario.getTelefone())
+                    .withClaim("genero", usuario.getGenero() != null ? usuario.getGenero().toString() : null)
+                    .withClaim("role", usuario.getRole().name()) // Adicionando role
                     .withExpiresAt(dataExpiracao())
                     .sign(algoritmo);
 
-        } catch (JWTCreationException exception){
+        } catch (JWTCreationException exception) {
+            logger.error(GENERATION_ERROR, exception);
             throw new RuntimeException(GENERATION_ERROR, exception);
         }
     }
 
-
-    public String getSubject(String tokenJWT){
+    public String getSubject(String tokenJWT) {
         try {
-            var algoritmo =  Algorithm.HMAC256(secret);
+            // Algoritmo para verificar o JWT
+            Algorithm algoritmo = Algorithm.HMAC256(secret);
+
+            // Verificação e recuperação do subject do JWT
             return JWT.require(algoritmo)
-                    .withIssuer("API Apolo Insights")
+                    .withIssuer(ISSUER)
                     .build()
                     .verify(tokenJWT)
                     .getSubject();
-        } catch (JWTVerificationException exception){
-            throw new RuntimeException("Token JWT inválido ou expirado!", exception);
+        } catch (JWTVerificationException exception) {
+            logger.error(TOKEN_ERROR, exception);
+            throw new RuntimeException(TOKEN_ERROR, exception);
         }
     }
 
     private Instant dataExpiracao() {
         return LocalDateTime.now().plusHours(expirationTime).toInstant(ZoneOffset.of("-03:00"));
     }
-
 }
