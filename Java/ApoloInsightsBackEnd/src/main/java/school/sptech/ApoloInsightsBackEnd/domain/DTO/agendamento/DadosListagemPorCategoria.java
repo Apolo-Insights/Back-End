de74f400e172cabf5e59873c9ca74dfea06dfe5e
JuanMeloOli <Juan.Oliveira@sptech.school>
2025-06-15 13:53:46 -0300

    idCategoria adicionado

--- Java/ApoloInsightsBackEnd/src/main/java/school/sptech/ApoloInsightsBackEnd/domain/DTO/agendamento/DadosListagemPorCategoria.java
+++ Java/ApoloInsightsBackEnd/src/main/java/school/sptech/ApoloInsightsBackEnd/domain/DTO/agendamento/DadosListagemPorCategoria.java
@@ -5,7 +5,7 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 public record DadosListagemPorCategoria(
-        Long idCategoria,
+        Long idAgendamento,
         String cliente,
         String servico,
         LocalDate data,
@@ -13,7 +13,8 @@ public record DadosListagemPorCategoria(
         Status status,
         String formaDePagamento
 ) {
-    public DadosListagemPorCategoria(String cliente,String servico, LocalDate data, LocalTime hora, Status status, String formaDePagamento) {
+    public DadosListagemPorCategoria(Long idAgendamento, String cliente,String servico, LocalDate data, LocalTime hora, Status status, String formaDePagamento) {
+        this.idAgendamento = idAgendamento;
         this.cliente = cliente;
         this.servico = servico;
         this.data = data;
