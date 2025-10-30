8b3fb8c6ddbbd93de49cfe5bc0244441ffb7a27c
JuanMeloOli <Juan.Oliveira@sptech.school>
2025-10-29 21:07:12 -0300

    atualizacao enum

--- Java/insightsApolo/src/main/java/apolov2/insightsApolo/v2/core/domain/entity/HorarioDisponivel.java
+++ Java/insightsApolo/src/main/java/apolov2/insightsApolo/v2/core/domain/entity/HorarioDisponivel.java
@@ -3,11 +3,13 @@ package apolov2.insightsApolo.v2.core.domain.entity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.DayOfWeek;
 import java.time.LocalTime;
 
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class HorarioDisponivel {
@@ -16,11 +18,13 @@ public class HorarioDisponivel {
     private DayOfWeek diaSemana;
     private LocalTime horaInicio;
     private LocalTime horaFim;
+    private Boolean bloqueado;
 
     public HorarioDisponivel(Categoria categoria, DayOfWeek diaSemana, LocalTime horaInicio, LocalTime horaFim) {
         this.categoria = categoria;
         this.diaSemana = diaSemana;
         this.horaInicio = horaInicio;
         this.horaFim = horaFim;
+        this.bloqueado = false; // Valor padrão
     }
 }
